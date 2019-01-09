9cbf391d00a7608a73db01a2eb58e7a6861c9e87
Ciaran Archer <carcher@zendesk.com>
2019-01-09 14:59:03 +0000

    Added validation for the grid.

--- sudoku_solver/solution.go
+++ sudoku_solver/solution.go
@@ -1,5 +1,27 @@
 package sudoku_solver
 
+import "fmt"
+
 func ValidateSolution(grid [][]int) bool {
-	return false
+	if !valid(grid) {
+		return false
+	}
+
+	return true
+}
+
+func valid(grid [][]int) bool {
+	if len(grid) == 0 || len(grid)%3 != 0 {
+		fmt.Println("incorrect grid size")
+		return false
+	}
+
+	for _, row := range grid {
+		if len(row) == 0 || len(row)%3 != 0 {
+			fmt.Println("incorrect row size")
+			return false
+		}
+	}
+
+	return true
 }
--- sudoku_solver/sudoku_solver_test.go
+++ sudoku_solver/sudoku_solver_test.go
@@ -32,8 +32,33 @@ var testFalse = [][]int{
 }
 
 var _ = Describe("Test Solution", func() {
-	It("should test that the solution returns the correct value", func() {
+	PIt("returns the correct value", func() {
 		Expect(ValidateSolution(testTrue)).To(Equal(true))
 		Expect(ValidateSolution(testFalse)).To(Equal(false))
+	})
+
+})
+
+var _ = Describe("a valid grid", func() {
+	It("rejects empty arrays", func() {
+		Expect(ValidateSolution([][]int{})).To(Equal(false))
+	})
+
+	It("rejects arrays where length is not a factor of 3", func() {
+		gridWrongRows := [][]int{
+			{5, 3, 4, 6, 7, 8, 9, 1, 2},
+			{6, 7, 2, 1, 9, 0, 3, 4, 8},
+			{1, 0, 0, 3, 4, 2, 5, 6, 0},
+			{8, 5, 9, 7, 6, 1, 0, 2, 0},
+		}
+
+		gridWrongCols := [][]int{
+			{5, 3, 4, 6, 7, 8, 9},
+			{6, 7, 2, 1, 9, 0, 3},
+			{1, 0, 0, 3, 4, 2, 5},
+		}
+
+		Expect(ValidateSolution(gridWrongRows)).To(Equal(false))
+		Expect(ValidateSolution(gridWrongCols)).To(Equal(false))
 	})
 })
