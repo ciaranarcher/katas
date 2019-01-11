7805ee3c065c545275eb82e6d434696ace92a7e8
Ciaran Archer <carcher@zendesk.com>
2019-01-11 11:29:50 +0000

    Working solution.

--- piano/piano.go
+++ piano/piano.go
@@ -1,6 +1,41 @@
 package piano
 
+const KeyboardLen int = 88
+const SectionLen int = 12
+const StartKeyboardLen int = 3
+const White string = "white"
+const Black string = "black"
+
 // BlackOrWhiteKey will return if a given key is black or white.
 func BlackOrWhiteKey(keyPressCount int) string {
-	return ""
+
+	// We know that we have an 88 key piano. So:
+	// 1) Figure out how many piano lengths there are.
+	// 2) Work out the remainder on an 88 key piano given it is comprised of repeating
+	// "sections" excpet for the start and the end of the keyboard.
+	keyPosition := keyPressCount % KeyboardLen
+
+	// Deal with start and end of the keyboard
+	// Remember we did a modulo with KeyboardLen!
+	if keyPosition == 0 { // End of the keyboard.
+		return White
+	} else if keyPosition <= 3 { // Start of the keyboard.
+		if keyPosition%2 == 0 {
+			return Black
+		}
+		return White
+	}
+
+	// Once we take away the first few keys, now we are dealing with a set of
+	// "sections". Each section has 12 keys. So mod by 12 to get the key
+	// position in the section.
+	keyPositionSection := (keyPosition - StartKeyboardLen) % SectionLen
+
+	// We know we have black keys at positions 2, 4, 7, 9 and 11.
+	switch keyPositionSection {
+	case 2, 4, 7, 9, 11:
+		return Black
+	default:
+		return White
+	}
 }
--- piano/piano_test.go
+++ piano/piano_test.go
@@ -9,7 +9,27 @@ import (
 
 var _ = Describe("Example tests", func() {
 	It("should test that the solution returns the correct value", func() {
+
+		// Deal with the edges
 		Expect(BlackOrWhiteKey(1)).To(Equal("white"))
+		Expect(BlackOrWhiteKey(2)).To(Equal("black"))
+		Expect(BlackOrWhiteKey(3)).To(Equal("white"))
+		Expect(BlackOrWhiteKey(88)).To(Equal("white"))
+
+		Expect(BlackOrWhiteKey(4)).To(Equal("white"))
+		Expect(BlackOrWhiteKey(5)).To(Equal("black"))
+		Expect(BlackOrWhiteKey(6)).To(Equal("white"))
+		Expect(BlackOrWhiteKey(7)).To(Equal("black"))
+		Expect(BlackOrWhiteKey(8)).To(Equal("white"))
+		Expect(BlackOrWhiteKey(9)).To(Equal("white"))
+		Expect(BlackOrWhiteKey(10)).To(Equal("black"))
+		Expect(BlackOrWhiteKey(11)).To(Equal("white"))
+		Expect(BlackOrWhiteKey(12)).To(Equal("black"))
+		Expect(BlackOrWhiteKey(13)).To(Equal("white"))
+		Expect(BlackOrWhiteKey(14)).To(Equal("black"))
+		Expect(BlackOrWhiteKey(15)).To(Equal("white"))
+		Expect(BlackOrWhiteKey(16)).To(Equal("white"))
+
 		Expect(BlackOrWhiteKey(5)).To(Equal("black"))
 		Expect(BlackOrWhiteKey(12)).To(Equal("black"))
 		Expect(BlackOrWhiteKey(42)).To(Equal("white"))
